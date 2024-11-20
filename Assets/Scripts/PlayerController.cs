3a2f3d1c48063340e23e89782ac6f2057dc92591
masonmclaughlin530 <165595276+masonmclaughlin530@users.noreply.github.com>
2024-11-20 16:22:31 -0500

    level loads

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -22,6 +24,33 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LoadPath01"))
+        {
+            //gameObject.transform.SetParent(noDestroy.transform);
+            //DontDestroyOnLoad(noDestroy);
+            SceneManager.LoadScene("Path01");
+
+        }
+        else if (collision.gameObject.CompareTag("LoadFriendHouse"))
+        {
+            SceneManager.LoadScene("FriendsHouse");
+        }
+        else if (collision.gameObject.CompareTag("LoadPath02"))
+        {
+            SceneManager.LoadScene("Path02");
+        }
+        else if (collision.gameObject.CompareTag("LoadCarePackage"))
+        {
+            SceneManager.LoadScene("CarePackage");
+        }
+        else if (collision.gameObject.CompareTag("LoadPath03"))
+        {
+            SceneManager.LoadScene("Path03");
+        }
+
+    }
 
 
 }
