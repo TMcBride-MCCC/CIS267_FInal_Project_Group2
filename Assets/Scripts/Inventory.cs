b950bfbce9cb6bb4db6d1bb1e356d6e96aff989b
TMcBride-MCCC <tmcbride2@my.monroeccc.edu>
2024-12-01 23:40:49 -0500

    InventoryUI
    Done. May need some tweaking for a more desirable look.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,18 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static event Action<List<InventoryItem>> OnInventoryChange;
+
+    public List<InventoryItem> inventory = new List<InventoryItem>();
+    private Dictionary<ItemData, InventoryItem> itemDictionary = new Dictionary<ItemData, InventoryItem>();
+
+    private void OnEnable()
     {
-        
+        Apple.OnAppleCollected += Add;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Apple.OnAppleCollected -= Add;
+    }
+
+    public void Add(ItemData itemData)
+    {
+        if (itemDictionary.TryGetValue(itemData, out InventoryItem item))
+        {
+            item.AddToStack();
+            Debug.Log(item.ItemData.displayName + " total stacksize is now " + item.stackSize);
+            OnInventoryChange?.Invoke(inventory);
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(itemData);
+            inventory.Add(newItem);
+            itemDictionary.Add(itemData, newItem);
+            Debug.Log(itemData.displayName + " was added to the inventory for the first time");
+            OnInventoryChange?.Invoke(inventory);
+        }
+    }
+
+    public void Remove(ItemData itemData)
+    {
+        if (itemDictionary.TryGetValue(itemData, out InventoryItem item))
+        {
+            item.RemoveFromStack();
+            if (item.stackSize == 0)
+            {
+                inventory.Remove(item);
+                itemDictionary.Remove(itemData);
+            }
+            OnInventoryChange?.Invoke(inventory);
+        }
     }
 }
