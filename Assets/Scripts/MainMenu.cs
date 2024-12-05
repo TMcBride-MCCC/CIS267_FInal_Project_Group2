20f06ca2ef905d768586a5da573f4b0944cf6345
masonmclaughlin530 <masonmclaughlin530@gmail.com>
2024-12-04 23:26:29 -0500

    MainMenuUpdate

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject firstButton;
+    public GameObject creditsClose;
+    public GameObject creditsOpen;
+
+    public GameObject menu;
+    public GameObject optionsMenu;
     public void PlayGame()
     {
         SceneManager.LoadScene("StartScene");
@@ -19,5 +26,34 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(firstButton);
+    }
+
+
+    public void creditsBtn()
+    {
+        optionsMenu.SetActive(true);
+        //menu.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(creditsOpen);
+    }
+
+    public void creditsCloseBtn()
+    {
+        optionsMenu.SetActive(false);
+        //menu.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(creditsClose);
     }
 }
